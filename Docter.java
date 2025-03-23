fb62473e0993a1ab0c7264d4868304a5ec1f0c24
Amrapali <amrapali707@gmail.com>
2025-03-23 21:25:17 +0530

    changes

---
+++ Docter.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+ class Docter{
+public static void main(String args[])
+{
+float D;
+float F;
+
+Scanner sc.new Scanner(System.in);
+System.out.println("Enter the degree:");
+d=sc.nextflaot();
+
+F=D*9/5+2;
+
+System.out.println(F);
+}
+}
